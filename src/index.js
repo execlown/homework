b049c52e72595cae895a9527eaf92612bfcabf51
Rizky Alwi <alwisebayang@gmail.com>
2022-04-08 09:59:32 +0700

    adding router

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import AppRouter from './router/AppRouter';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  (document.getElementById("root"))
+);
